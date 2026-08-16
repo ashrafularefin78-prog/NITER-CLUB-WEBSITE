25771b4a81fb42f95dbe2797f8dfeafb75c5ed78
Ashraful <ashrafularefin78@gmail.com>
2026-08-17 01:05:59 +0600

    Upload project source files

--- .freebuff/docx-work/inspect6.ps1
+++
@@ -1,7 +0,0 @@
-$ErrorActionPreference = "Stop"
-$base = "C:\Users\user\Downloads\Clubs\.freebuff\docx-work\x"
-$x = [IO.File]::ReadAllText("$base\word\document.xml")
-$start = $x.IndexOf('<w:tbl>', 12900)
-$end = $x.IndexOf('</w:tbl>', $start)
-Write-Host ("table from " + $start + " to " + ($end + 8))
-Write-Host ($x.Substring($start, $end + 8 - $start))
